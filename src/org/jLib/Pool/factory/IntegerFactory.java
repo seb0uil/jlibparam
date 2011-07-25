5461cdb29ebcc5d8577da4a3bf3905dfb4bac79d
seb0uil@gmail.com <seb0uil@gmail.com@52bf8189-340c-13f8-0d1f-8bb2d1731ee4>
2011-07-25 22:44:30 +0000

    Debut ajout de timeout sur le pool

--- src/org/jLib/Pool/factory/IntegerFactory.java
+++ src/org/jLib/Pool/factory/IntegerFactory.java
@@ -5,5 +5,9 @@ public class IntegerFactory implements IPoolFactory<Integer> {
 	public Integer create() {
 		return new Integer(0);
 	}
+	
+	public void expire(Integer obj) {
+		System.out.println(obj + "  expire..");
+	}
 
 }
